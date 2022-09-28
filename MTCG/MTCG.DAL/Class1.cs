adc5e159db0a53c10c6ce76e43a394a1d2fed8a0
Simon Penn <smnpenn@gmail.com>
2022-09-28 10:42:21 +0200

    layered structure + classes

---
+++ MTCG/MTCG.DAL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MTCG.DAL
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ MTCG/MTCG.Model/Card.cs
@@ -0,0 +1,27 @@
+﻿namespace MTCG.Model
+{
+
+    public enum ElementType
+    {
+        normal = 0,
+        fire = 1,
+        water = 2
+
+    }
+
+    public class Card
+    {
+        private int damage;
+        private string name;
+        private ElementType element;
+
+
+        public Card(string name, int damage, ElementType element)
+        {
+            this.name = name;
+            this.damage = damage;
+            this.element = element;
+        }
+        public int Damage { get; }
+    }
+}
